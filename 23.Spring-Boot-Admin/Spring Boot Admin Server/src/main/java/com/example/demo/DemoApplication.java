0fc178acb8198168bd633ae1632fcac96a604291
acpmbe <acpmbe@qq.com>
2020-05-14 13:45:04 +0800

    上传springAdmin测试代码
    上传springAdmin测试代码

--- 23.Spring-Boot-Admin/Spring Boot Admin Server/src/main/java/com/example/demo/DemoApplication.java
+++ 23.Spring-Boot-Admin/Spring Boot Admin Server/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+
 
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableAdminServer
 public class DemoApplication {
 
